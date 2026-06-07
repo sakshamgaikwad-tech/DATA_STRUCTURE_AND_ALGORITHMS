649978fbef88f88b67b94b128cedb38951ba5b99
Saksham Gaikwad <sakshamgaikwad1377@gmail.com>
2026-06-07 18:51:30 +0530

    Create problem_no_2196.cpp

---
+++ problem_no_2196.cpp
@@ -0,0 +1,55 @@
+// QUESION : You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+// If isLefti == 1, then childi is the left child of parenti.
+// If isLefti == 0, then childi is the right child of parenti.
+// Construct the binary tree described by descriptions and return its root.
+
+// The test cases will be gen
+// https://leetcode.com/problems/create-binary-tree-from-descriptions/erated such that the binary tree is valid.
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> nodes;
+        unordered_set<int> children;
+
+        for (auto& d : descriptions) {
+            int parent = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+
+            if (!nodes.count(parent))
+                nodes[parent] = new TreeNode(parent);
+
+            if (!nodes.count(child))
+                nodes[child] = new TreeNode(child);
+
+            if (isLeft)
+                nodes[parent]->left = nodes[child];
+            else
+                nodes[parent]->right = nodes[child];
+
+            children.insert(child);  
+        }
+
+        for (auto& [val, node] : nodes) {
+            if (!children.count(val))
+                return node;
+        }
+
+        return nullptr;
+    }
+};
