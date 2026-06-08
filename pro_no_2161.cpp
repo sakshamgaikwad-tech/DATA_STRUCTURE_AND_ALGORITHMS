b0474b8e8ffbfa0cb52185bbf39c32a02cef7380
Saksham Gaikwad <sakshamgaikwad1377@gmail.com>
2026-06-08 20:11:23 +0530

    Create pro_no_2161.cpp

---
+++ pro_no_2161.cpp
@@ -0,0 +1,37 @@
+// problem 2161: 
+
+
+// You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+// Every element less than pivot appears before every element greater than pivot.
+// Every element equal to pivot appears in between the elements less than and greater than pivot.
+// The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+// More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. If i < j and both elements are smaller (or larger) than pivot, then pi < pj.
+// Return nums after the rearrangement.
+
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+
+        vector<int> l, e, g;
+
+        for (int n : nums) {
+            if (n < pivot) {
+                l.push_back(n);
+            }
+
+            else if (n == pivot) {
+                e.push_back(n);
+            } else {
+                g.push_back(n);
+            }
+        }
+        vector<int> result;
+        result.insert(result.end(), l.begin(), l.end());
+        result.insert(result.end(), e.begin(), e.end());
+        result.insert(result.end(), g.begin(), g.end());
+
+        return result;
+    }
+};
