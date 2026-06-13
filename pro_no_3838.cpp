5f4e971238fd64f3afdf2d5f28e73d9f2556e934
Saksham Gaikwad <sakshamgaikwad1377@gmail.com>
2026-06-13 11:27:39 +0530

    Add initial comments and function definition for word mapping

---
+++ pro_no_3838.cpp
@@ -0,0 +1,45 @@
+// You are given an array of strings words, where each string represents a word containing lowercase English letters.
+
+// You are also given an integer array weights of length 26, where weights[i] represents the weight of the ith lowercase English letter.
+
+// The weight of a word is defined as the sum of the weights of its characters.
+
+// For each word, take its weight modulo 26 and map the result to a lowercase English letter using reverse alphabetical order (0 -> 'z', 1 -> 'y', ..., 25 -> 'a').
+
+// Return a string formed by concatenating the mapped characters for all words in order.
+
+ 
+
+// Example 1:
+
+// Input: words = ["abcd","def","xyz"], weights = [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2]
+
+// Output: "rij"
+
+
+
+
+
+
+
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string res;
+        int n = words.size();
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < words[i].size(); j++) {
+                char ch = words[i][j];
+                sum += weights[ch - 'a'];
+            }
+
+            int r = sum % 26;
+            res += char('z' - r);
+        }
+
+        return res;
+    }
+};
