7ae14e9209e2d7b049b94d3125a6c1ddbfce2356
Saksham Gaikwad <sakshamgaikwad1377@gmail.com>
2026-06-15 17:48:04 +0530

    Update problem_no_2196.cpp

--- problem_no_2196.cpp
+++ problem_no_2196.cpp
@@ -8,18 +8,27 @@
 // https://leetcode.com/problems/create-binary-tree-from-descriptions/erated such that the binary tree is valid.
 
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
@@ -42,14 +51,42 @@ public:
             else
                 nodes[parent]->right = nodes[child];
 
-            children.insert(child);  
+            children.insert(child);
         }
 
         for (auto& [val, node] : nodes) {
             if (!children.count(val))
-                return node;
+                return node; // Root node
         }
 
         return nullptr;
     }
 };
+
+// Inorder Traversal
+void inorder(TreeNode* root) {
+    if (!root) return;
+
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Solution obj;
+
+    vector<vector<int>> descriptions = {
+        {20, 15, 1},
+        {20, 17, 0},
+        {15, 10, 1},
+        {15, 12, 0}
+    };
+
+    TreeNode* root = obj.createBinaryTree(descriptions);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
