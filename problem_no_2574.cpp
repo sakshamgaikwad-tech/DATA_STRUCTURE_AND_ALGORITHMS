40700ea7219f96ce76575cf40028100b7af1fbc0
Saksham Gaikwad <sakshamgaikwad1377@gmail.com>
2026-06-15 17:47:15 +0530

    Update problem_no_2574.cpp

--- problem_no_2574.cpp
+++ problem_no_2574.cpp
@@ -5,10 +5,15 @@
 // leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
 // rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
 // Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
+#include <iostream>
+#include <vector>
+#include <cmath>
 
- class Solution {
+using namespace std;
+
+class Solution {
 public:
-    vector<int> leftRigthDifference(vector<int>& nums) {
+    vector<int> leftRightDifference(vector<int>& nums) {
         int n = nums.size();
         vector<int> answer(n);
 
@@ -20,12 +25,29 @@ public:
         int leftSum = 0;
 
         for (int i = 0; i < n; i++) {
-            totalSum -= nums[i];
-          
+            totalSum -= nums[i];  // Right sum
+
             answer[i] = abs(leftSum - totalSum);
+
             leftSum += nums[i];
         }
 
         return answer;
     }
 };
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {10, 4, 8, 3};
+
+    vector<int> result = obj.leftRightDifference(nums);
+
+    cout << "Output: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
